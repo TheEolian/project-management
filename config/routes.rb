4a509f1b03476d71f03a77fcaddaa123416105d2
theeolian <msoldmann@gmail.com>
2017-04-13 03:41:38 +0000

    Finish with Styling and Structure

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   
   resources :projects do
     resources :tasks do
-      member do
-        patch :complete
-      end
+    	member do 
+    		patch :complete
+    	end
     end
   end
+
+  root "projects#index"
 end
